e165e266f917e60685f91152963786b6cdde45e7
DogmaIAmGod <50518897+DogmaIAmGod@users.noreply.github.com>
2024-08-16 02:37:04 -0400

    Update testSend.py

--- testSend.py
+++ testSend.py
@@ -1,7 +1,4 @@
-import pyautogui
 import time
-
-pyautogui.FAILSAFE = True
 
 with open('array.txt', 'r') as file:
     array = [line.strip() for line in file]
@@ -10,6 +7,6 @@ with open('array.txt', 'r') as file:
 async def test_send(message) -> None:
     time.sleep(2)
     for item in array:
-        dynamic_message = '<@235148962103951360> roles <@' + item + '>\n'
+        dynamic_message = '<@235148962103951360> doThing <@' + item + '>\n'
         await message.channel.send(dynamic_message)
         time.sleep(.5)
